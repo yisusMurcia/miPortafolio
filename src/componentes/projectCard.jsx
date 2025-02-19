bb588d76e0c188757853242332078cce8374f33c
Jesus Antonio Murcia <yisusmurci@gmail.com>
2025-02-19 10:25:00 -0500

    Enlace a los proyectos añadidos -Se añadió el eventListener para redirigirse a los proyectos. -Se modifico el estilo de las tarjetas de proyectos para hacerla más intuitiva

--- src/componentes/projectCard.jsx
+++ src/componentes/projectCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProjectCard = ({projectObj}) => {
     return (
-        <div className='project-card'>
+        <div className='project-card' onClick={()=> window.open(projectObj.enlace, "_blank")}>
             <img src={projectObj?.img? "public/projects-img/"+ projectObj.img: "public/projects-img/default.jpeg"} alt={projectObj.titulo} />
             <h3>{projectObj.titulo}</h3>
             <p>{projectObj.descripcion}</p>
