f7dbf12b0386ca957e7a69ed0f4cbedb38eedb08
Jesus Antonio Murcia <yisusmurci@gmail.com>
2025-02-28 15:36:02 -0500

    section simplified

--- src/componentes/AboutMeSection.jsx
+++ src/componentes/AboutMeSection.jsx
@@ -6,25 +6,13 @@ const AboutMeSection = () => {
         <section>
             <h2>Sobre mí</h2>
             <aside className='about-me-container'>
-                <div>
-                    <h2>Hablidades blandas</h2>
-                    <ul id="habilidades-blandas">
-                        <li>Resolución de problemas</li>
-                        <li>Comunicación</li>
-                        <li>Gestión de conflictos</li>
-                        <li>Organización bajo presión</li>
-                    </ul>            
-                </div>
-                <img src="yisus.jpg" alt="yisus pic"/>
                 <p>
-                Soy un apasionado y dedicado a la programación, siempre buscando ser la mejor versión de mí mismo.
-                Mi interés se extiende a la ciencia, en particular al espacio y la física cuántica.
-                Mi memoria y capacidad de pensamiento lógico me permiten resolver problemas de manera analítica,
-                sin dejar de lado la creatividad. Disfruto trabajar de forma independiente, pero también tengo experiencia liderando
-                equipos para alcanzar objetivos comunes. Abordo los desafíos con calma, siempre en busca de soluciones funcionales y
-                optimizándolas a medida que avanzo. Mi mayor miedo es caer en una vida monótona y aburrida, lo que me motiva a
-                transformar mi pasión por la programación en una carrera profesional dinámica y emocionante.
+                    Apasionado por la programación y la ciencia, siempre en busca de crecimiento y nuevos desafíos.
+                    Destaco por mi pensamiento lógico y analítico, sin dejar de lado la creatividad. Disfruto trabajar de forma independiente,
+                    pero también sé liderar equipos para alcanzar objetivos.
+                    Mi mayor miedo es la monotonía, por eso busco una carrera dinámica que me rete constantemente.
                 </p>
+                <img src="yisus.jpg" alt="yisus pic"/>
                 <span className="more-info">
                 Conoce más de mí:
                 <a href="https://bento.me/yisusm">
