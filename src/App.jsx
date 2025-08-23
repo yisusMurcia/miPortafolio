8c447689c5ece618802a0ad6fb950741451f21c6
Jesus Antonio Murcia <yisusmurci@gmail.com>
2025-08-23 15:56:21 -0500

    Load projects updfated -Added obj distinction on the "enlace" of proyects for more complex projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import { useState, useEffect } from 'react';
 import Header from './componentes/Header';
 import './App.css';
-import getProyects from './api/getProyects';
 import ProjectsSection from './componentes/projectsSection';
 import AboutMeSection from './componentes/AboutMeSection';
+import projects from './hooks/projects';
 
 function App() {
-  const [projects, setProjects] = useState(null);
-  //Load proyects
-  async function loadProjects() {
-    try {
-      setProjects(await getProyects());
-    } catch (error) {//Manejar error en caso de que no se pueda cargar los proyectos
-      console.error("Error en loadProjects:", error);
-      return null;
-    }
-  }
-
-
-  useEffect(() => {
-    loadProjects();
-  }, []);
 
   return (
     <>
--- src/componentes/projectCard.jsx
+++ src/componentes/projectCard.jsx
@@ -3,10 +3,18 @@ import TechComponent from './techComponent';
 
 const ProjectCard = ({projectObj}) => {
     return (
-        <div className='project-card' onClick={()=> window.open(projectObj.enlace, "_blank")}>
+        <div className='project-card'>
             <img src={"projects-img/" + (projectObj?.img ? projectObj.img: "default.jpeg")} alt={projectObj.titulo} />
             <h3>{projectObj.titulo}</h3>
             <p>{projectObj.descripcion}</p>
+            {
+                typeof(projectObj.enlace) == "object"?
+                <div>
+                    {Object.keys(projectObj.enlace).map(key=><a href={projectObj.enlace.key} target="_blank" rel="noopener noreferrer" key={key}>{key}</a>)}
+                </div>
+                :
+                <a href={projectObj.enlace} target="_blank" rel="noopener noreferrer">Ver proyecto</a>
+            }
             <TechComponent techs={projectObj.tecnologias}/>
         </div>
     );
